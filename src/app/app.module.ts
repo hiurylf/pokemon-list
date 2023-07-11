cbc8ff50647788645a21c81119e6aa4ce08e85ac
M261485 <Mjv@4321>
2023-07-10 23:00:10 -0300

    feat: add ng-bootstrap and HttpClient/ReactiveForms modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { ListComponent } from './pages/list/list.component';
 
 @NgModule({
 	declarations: [AppComponent, ListComponent],
-	imports: [BrowserModule, AppRoutingModule],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		NgbModule,
+		HttpClientModule,
+		ReactiveFormsModule,
+		FormsModule,
+	],
 	providers: [],
 	bootstrap: [AppComponent],
 })
