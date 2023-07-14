6922d98f7b000a100256e4a9f6d6748b1253e134
M261485 <Mjv@4321>
2023-07-14 06:56:52 -0300

    feat: add components to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,22 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ListComponent } from './pages/list/list.component';
+import { ListComponent } from '@pages/list/list.component';
+import { ListItemComponent } from '@components/list-item/list-item.component';
+import { DetailComponent } from '@components/detail/detail.component';
+import { ModalWrapperComponent } from '@components/modal-wrapper/modal-wrapper.component';
+
+import { StoreModule } from '@ngrx/store';
+import { pokemonListReducer } from '@app/store/pokemon-list.reducer';
 
 @NgModule({
-	declarations: [AppComponent, ListComponent],
+	declarations: [
+		AppComponent,
+		ListComponent,
+		ListItemComponent,
+		DetailComponent,
+		ModalWrapperComponent,
+	],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
