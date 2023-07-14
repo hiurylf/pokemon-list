61df8a55741e1cbf29714053e588b6517a02828a
M261485 <Mjv@4321>
2023-07-14 06:57:11 -0300

    feat: add detail router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ListComponent } from './pages/list/list.component';
+import { ListComponent } from '@pages/list/list.component';
+import { ModalWrapperComponent } from '@components/modal-wrapper/modal-wrapper.component';
+import { DetailComponent } from '@components/detail/detail.component';
 
-const routes: Routes = [{ path: '**', component: ListComponent }];
+const routes: Routes = [
+	{
+		path: 'home',
+		component: ListComponent,
+		children: [
+			{
+				path: ':id',
+				component: ModalWrapperComponent,
+				data: { component: DetailComponent },
+			},
+		],
+	},
+	{ path: '**', redirectTo: 'home' },
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
